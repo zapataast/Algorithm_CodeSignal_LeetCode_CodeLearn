863ca6687b6fb8add61dc35b4229a73f35cd94c7
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2021-03-27 20:01:33 +0800

    Databases 20 GROUP BY , HAVING
    For the following table moviesInfo
    
    title	director	year	oscars
    BoBoiBoy: The Movie	Nizam Razak	2016	0
    Inception	Christopher Nolan	2010	4
    Interstellar	Christopher Nolan	2014	1
    Munna Bhai M.B.B.S.	Rajkumar Hirani	2003	0
    My Dear Brother	Ertem Egilmez	1973	0
    Rocky John	G. Avildsen	1976	3
    The Nights of Cabiria	Federico Fellini	1957	1
    The Sixth Sense	M. Night Shyamalan	1999	6
    The Sixth Sense	M. Night Shyamalan	1999	6
    Tokyo Story	Yasujirô Ozu	1953	0
    Yojimbo	Akira Kurosawa	1961	1
    the output should be
    
    director
    Christopher Nolan

---
+++ movieDirectors.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE movieDirectors()
+BEGIN
+    SELECT director
+    FROM moviesInfo 
+    WHERE year > 2000
+    GROUP BY director 
+    HAVING SUM(oscars) >2
+    ORDER BY director
+    ;
+END
