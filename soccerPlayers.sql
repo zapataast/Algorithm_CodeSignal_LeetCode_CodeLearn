c644bb116e8fecfb03f6e5b4b4bd0fe573488e0f
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2021-03-27 21:01:10 +0800

    Databases 22 GROUP_CONCAT
    GROUP_CONCAT болон CONCAT ашиглаж баганан мэдээллүүдийг нэг мөрөнд оруулав.

---
+++ soccerPlayers.sql
@@ -0,0 +1,6 @@
+CREATE PROCEDURE soccerPlayers()
+BEGIN
+   	SELECT GROUP_CONCAT(CONCAT(first_name," ",surname, " #",player_number) ORDER BY player_number SEPARATOR '; ') AS players
+    FROM soccer_team
+    ;
+END
