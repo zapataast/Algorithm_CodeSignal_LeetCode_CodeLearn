648adbd2be0fb4b1f92186f8f3e64eb097bb263e
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2021-04-07 15:59:13 +0800

    Databases 30 SUBSTRING , XML file, LOCATE function
    #SELECT LOCATE("com", "W3Schools.com",3) AS MatchPosition;  гэвэл W3Schools.com-оос index буцаана. 1ээс эхэлж тоолвол 11 гэсэн утга буцаана
    #SELECT SUBSTRING(CustomerName, 1, 9) AS ExtractString FROM Customers; гэвэл 1 -ээс 9 дүгээр index хоорондох үгийг салгаж харуулна.

--- booksCatalogs.sql
+++ booksCatalogs.sql
@@ -1,8 +1,7 @@
 CREATE PROCEDURE booksCatalogs()
 BEGIN
-    SELECT SUBSTRING(xml_doc, 
-    LOCATE(CONCAT('<', 'author', '>'), xml_doc) + LENGTH(CONCAT('<', 'author', '>')),
-    (LOCATE(CONCAT('</', 'author', '>'),xml_doc))-(LOCATE(CONCAT('<', 'author', '>'), xml_doc) + LENGTH(CONCAT('<', 'author', '>'))))    AS gg
+    SELECT   
+    SUBSTRING(xml_doc, LOCATE('<author>',xml_doc)+8, LOCATE('</author>', xml_doc)-LOCATE('<author>',xml_doc)-8) AS gg 
     FROM catalogs
     ORDER BY gg
     ;
