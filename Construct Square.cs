938333b55c0e7b0a535e0e474b450c2ccbdb42b9
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2021-04-30 17:57:58 +0800

    Core 62 Dictionary аргууд
    Given a string consisting of lowercase English letters, find the largest square number which can be obtained by reordering the string's characters and replacing them with any digits you need (leading zeros are not allowed) where same characters always map to the same digits and different characters always map to different digits.
    
    If there is no solution, return -1.
    
    Example
    
    For s = "ab", the output should be
    constructSquare(s) = 81.
    The largest 2-digit square number with different digits is 81.
    For s = "zzz", the output should be
    constructSquare(s) = -1.
    There are no 3-digit square numbers with identical digits.
    For s = "aba", the output should be
    constructSquare(s) = 900.
    It can be obtained after reordering the initial string into "baa" and replacing "a" with 0 and "b" with 9.
    Input/Output
    
    [execution time limit] 3 seconds (cs)
    
    [input] string s
    
    Guaranteed constraints:
    1 ≤ s.length < 10.

---
+++ Construct Square.cs
@@ -0,0 +1,57 @@
+int constructSquare(string s) {
+    int maxvalue = 10;
+    Dictionary<string, int> My_dict1 =  new Dictionary<string, int>(); 
+    for(int i=0; i<s.Length-1;i++){ // гарч болох хамгийн дээд тоог тодорхойлж байна.99 , 999 гэх мэт
+        maxvalue *= 10;
+    } maxvalue--;
+    int startvalue = Convert.ToInt32(Math.Sqrt(maxvalue)); // эхлэх хамгийн бага утгыг тодорхойлж байна
+    
+    Console.WriteLine("maxvalue: "+ maxvalue);
+    Console.WriteLine("startvalue:" +startvalue);
+    
+    for(int i=0; i<s.Length; i++){ // S үгийн үсгүүдийг тоолж Dictionary-д оруулж байна
+        if (My_dict1.ContainsKey(s[i].ToString())==true){
+            int b1 = My_dict1[s[i].ToString()]+1;
+            My_dict1[s[i].ToString()] = b1;
+        }else{
+            My_dict1.Add(s[i].ToString(), 1);
+        }
+    }
+    
+    for(; 0<startvalue; startvalue--){
+        Dictionary<string, int> dict_num =  new Dictionary<string, int>();
+        string temp = Convert.ToInt32(Math.Pow(startvalue,2)).ToString();
+        Console.WriteLine("temp: " + temp);
+        for(int i=0; i<temp.Length; i++){ // квадрат тоо болгоныг Dictionary-д түр хадгалж байна
+            if(dict_num.ContainsKey(temp[i].ToString()) == true){
+                int b1 = dict_num[temp[i].ToString()]+1;
+                dict_num[temp[i].ToString()] = b1;
+            }else{
+                dict_num.Add(temp[i].ToString(), 1);
+            }
+        } 
+        int taaralt = 0;
+        
+        for(int j=0; j<My_dict1.Count; j++){
+            
+            if (dict_num.ContainsValue(My_dict1[My_dict1.ElementAt(j).Key])){ //My_dict1- утга dict_num-д байгаа үгүйг шалгаж байна
+                taaralt++;  
+                int catched = My_dict1[My_dict1.ElementAt(j).Key]; // My_dict1 value-ыг хадгалж байна
+                
+                Console.WriteLine("t1");
+                
+                var key1 = dict_num.FirstOrDefault(x => x.Value == catched).Key; //дээрх утгыг ашиглан dict_num-ын key -ыг олж авч байна
+                
+                if (key1 != null) {
+                    Console.WriteLine("Key \"{0}\" value \"{1}\"", key1, catched);
+                }
+                dict_num.Remove(key1); // key ашиглан устгаж байна, давхардлыг арилгах зорилготой.
+            }
+           
+        }
+        if(taaralt==My_dict1.Count && dict_num.Count==0){
+            return Int32.Parse(temp);
+        }
+    }
+    return -1;
+}
