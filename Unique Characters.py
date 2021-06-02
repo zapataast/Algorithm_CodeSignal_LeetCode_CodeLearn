db3003a309081e1dfb9893094ad4eaba498a77ba
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2021-06-02 11:34:58 +0800

    Python 41 set function , sorted function , list
    You need to compress a large document that consists of a small number of different characters. To choose the best encoding algorithm, you would like to look closely at the characters that comprise this document.
    
    Given a document, return an array of all unique characters that appear in it sorted by their ASCII codes.
    
    Example
    
    For document = "Todd told Tom to trot to the timber",
    the output should be
    uniqueCharacters(document) = [' ', 'T', 'b', 'd', 'e', 'h', 'i', 'l', 'm', 'o', 'r', 't'].

---
+++ Unique Characters.py
@@ -0,0 +1,5 @@
+def uniqueCharacters(document):
+    return sorted(list(set(document))) 
+    #sorted // list утгуудыг sort-лож байна
+    #list   // list-тэнд хадгалж байна
+    #set    // үгэнд орсон бүх давтагдаж байгаа үсгүүдийг хасаад set type-нд хадгалж байна
