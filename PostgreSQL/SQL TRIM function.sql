dc7d504a8576f2ac1969ea756f3d4e30c7a78dbe
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2021-09-25 17:00:23 +0800

    PostgreSQL 20 REGEXP_REPLACE , TRIM
    id (SMALLINT) : id of the title.
    title (TEXT) : name of the article.
    Example
    For the data of the following table:
    
    titles
    
    id	title
    1	    advantages of        Python Compared    to C and Java?      
    2	    how to MANIPULATE      a 2-dimensional     array in   Javascript?     
    3	Delegate, Anonymous Method and Lambda Expression In C 
    4	8 TECHNOLOGY TRENDS Cant Be Missed             2020                 
    5	codelearn                   codelearn             codelearn
    6	     SQL LEFT FUNCTION              
    The output should be:
    
    id	title
    1	Advantages Of Python Compared To C And Java?
    2	How To Manipulate A 2-Dimensional Array In Javascript?
    3	Delegate, Anonymous Method And Lambda Expression In C
    4	8 Technology Trends Cant Be Missed 2020
    5	Codelearn Codelearn Codelearn
    6	Sql Left Function
    Execution time limit: 0.5 second (PosgreSQL)

--- PostgreSQL/SQL TRIM function.sql
+++ PostgreSQL/SQL TRIM function.sql
@@ -3,9 +3,9 @@ You may not modify the pre-existing code. */
 
 SELECT 
 	id AS id, 
-	-- Format the value of title column
 	INITCAP(REGEXP_REPLACE(TRIM(BOTH title),'\s+',' ','g')) AS title 
-  -- REGEXP_REPLACE функц нь үгнүүдийн хоорондох хоосон зайнуудыг устгаж 1 л хоосон зай үлдээдэг.
-  -- TRIM Сонгогдсон үгсийн баруун болон зүүн талын хоосон зайнуудыг устгадаг
-    -- LEADING | TRAILING | BOTH гэсэн 3 сонголттой
+  	-- REGEXP_REPLACE функц нь үгнүүдийн хоорондох хоосон зайнуудыг устгаж 1 л хоосон зай үлдээдэг.
+  	-- TRIM Сонгогдсон үгсийн баруун болон зүүн талын хоосон зайнуудыг устгадаг
+	-- LTRIM('4723/1 MySakila Drive','1234567890/#. ')  -- гэвэл эхний үгний зүүн талаас нь устгаж гаралтанд: 'MySakila Drive' гэж гарна.
+    	-- LEADING | TRAILING | BOTH гэсэн 3 сонголттой
 FROM titles;
