c4b0b469fff6ae7a5fcf2fc4f13f32e7097a8d22
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2021-10-21 10:15:37 +0800

    Databases 37 REGEXP , WHERE Advanced , isdigit
    Your first task as a company employee is to retrieve all the valid records from the given table of phone numbers. The record is valid if its phone_number is a valid American or Canadian phone number, which means that it has a format of 1-###-###-#### or (1)###-###-####, where # stands for any digit from 0 to 9.
    
    Information is stored in the table phone_numbers, which has the following fields:
    
    name: the user's first name;
    surname: the user's unique surname;
    phone_number: the user's unique phone number.
    You should return a new table that contains only the valid records from the phone_numbers table, sorted by the users' surnames.
    
    Example
    
    For the following table phone_numbers
    
    name	surname	phone_number
    Cornelius	Walsh	1-234-567-8910
    Frank	McKenzie	1-2345-678-911
    John	Smith	(1)111-111-1111
    Lester	Goodwin	(1)-111-111-1111
    the output should be
    
    name	surname	phone_number
    John	Smith	(1)111-111-1111
    Cornelius	Walsh	1-234-567-8910

---
+++ Databases/validPhoneNumbers.sql
@@ -0,0 +1,34 @@
+CREATE PROCEDURE validPhoneNumbers()
+BEGIN
+    /* American or Canadion Phone number STANDART */
+	Select *
+    FROM phone_numbers
+    
+    #(1)###-###-#### 
+    WHERE phone_number REGEXP '^\\(1\\)[0-9]{3}-[0-9]{3}-[0-9]{4}$'
+    OR
+    #1-###-###-#### 
+phone_number REGEXP '^1-[0-9]{3}-[0-9]{3}-[0-9]{4}$'
+    ORDER BY surname 
+    ;
+END
+
+/*
+*	Zero or more instances of string preceding it
++	One or more instances of strings preceding it
+.	Any single character
+?	Match zero or one instances of the strings preceding it.
+^	caret(^) matches Beginning of string
+$	End of string
+[abc]	Any character listed between the square brackets
+[^abc]	Any character not listed between the square brackets
+[A-Z]	match any upper case letter.
+[a-z]	match any lower case letter
+[0-9]	match any digit from 0 through to 9.
+[[:<:]]	matches the beginning of words.
+[[:>:]]	matches the end of words.
+[:class:]	matches a character class i.e. [:alpha:] to match letters, [:space:] to match white space, [:punct:] is match punctuations and [:upper:] for upper class letters.
+p1|p2|p3	Alternation; matches any of the patterns p1, p2, or p3
+{n}	n instances of preceding element
+{m,n}	m through n instances of preceding element
+*/
