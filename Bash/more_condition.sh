e9f3880ff48ce881d77631b62d4d8d58d704ec82
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2022-01-25 09:23:08 +0800

    More on Conditionals if else
    Given three integers (, , and ) representing the three sides of a triangle, identify whether the triangle is scalene, isosceles, or equilateral.
    
    If all three sides are equal, output EQUILATERAL.
    Otherwise, if any two sides are equal, output ISOSCELES.
    Otherwise, output SCALENE.
    Input Format
    
    Three integers, each on a new line.
    
    Constraints
    
    
    The sum of any two sides will be greater than the third.
    
    Output Format
    
    One word: either "SCALENE" or "EQUILATERAL" or "ISOSCELES" (quotation marks excluded).
    
    Sample Input
    
    Sample Input 1
    
    2
    3
    4
    Sample Input 2
    
    6
    6
    6  
    Sample Output
    
    Sample Output 1
    
    SCALENE
    Sample Output 2
    
    EQUILATERAL

---
+++ Bash/more_condition.sh
@@ -0,0 +1,12 @@
+read a 
+read b 
+read c 
+if [ $a == $b ] && [ $b == $c ] 
+    then 
+    echo 'EQUILATERAL'
+    elif [ $a == $b ] || [ $a == $c ] || [ $c == $b ] 
+    then 
+    echo 'ISOSCELES'
+    else
+    echo 'SCALENE'
+fi
