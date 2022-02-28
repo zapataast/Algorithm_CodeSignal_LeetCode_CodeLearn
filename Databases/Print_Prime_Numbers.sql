cbb259e9eddda1f57154e8f757231fe1af3b6106
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2022-02-28 18:09:12 +0800

    Hackerrank - IF, LOOP, WHILE, DECLARE MSSQL
    Write a query to print all prime numbers less than or equal to . Print your result on a single line, and use the ampersand () character as your separator (instead of a space).
    
    For example, the output for all prime numbers  would be:
    
    2&3&5&7

--- Databases/Print_Prime_Numbers.sql
+++ Databases/Print_Prime_Numbers.sql
@@ -1,26 +1,24 @@
-DECLARE @var int, @div int, @prime int      
+DECLARE @var int , @prime int      
 DECLARE @STR varchar(700) = '2' -- Declare
 SELECT @var = 1                -- Initialization
 SELECT @prime = 2
-SET @div = 2;
+
 
 WHILE @var <= 1000                 -- condition
     BEGIN                          -- Begin
     WHILE @prime < @var 
         BEGIN 
-        IF @var % @div = 0 
+        IF @var % @prime = 0 
             BEGIN 
             break
             END
-        IF @var-1 = @div
+        IF @var-1 = @prime
             BEGIN
                 SET @STR = CONCAT(@STR,'&',CAST(@var as varchar))
             END
         SET @prime = @prime + 1
-        SET @div = @div + 1
     END
     SET @var = @var + 1            -- decrement
-    SET @div = 2
     SET @prime = 2
 END
 
