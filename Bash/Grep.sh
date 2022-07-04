d9e8ad62d0ccadb3b47b211d8aa39e31adce830f
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2022-07-04 13:36:13 +0800

    Update Grep.sh

--- Bash/Grep.sh
+++ Bash/Grep.sh
@@ -1,2 +1,3 @@
 grep -Eiw 'the|those|then|that' filename.txt # filename.txt-нд the,those,then,that үгнүүд орсон мөр байвал туxайн мөрийг бүтнээр нь хэвлэж байна 
 grep -Eiw 'th(e|ose|en|at)' filename.txt # дээрхтэй ижил 
+grep '\(\d\)\s*\1' filename.txt # тухайн мөрөнд давтагдсан тоо байвал үлдээнэ жишээ нь  2234 хоёрын тоо дараалан давтагдсан байна. 
