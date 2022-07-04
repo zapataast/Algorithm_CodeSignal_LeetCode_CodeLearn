4440318556be391b49ba32299641e4827084c96e
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2022-07-04 12:43:30 +0800

    introducing grep

---
+++ Bash/Grep.sh
@@ -0,0 +1,2 @@
+grep -Eiw 'the|those|then|that' filename.txt # filename.txt-нд the,those,then,that үгнүүд орсон мөр байвал туxайн мөрийг бүтнээр нь хэвлэж байна 
+grep -Eiw 'th(e|ose|en|at)' filename.txt # дээрхтэй ижил 
