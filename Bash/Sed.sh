8cf038a80e56cf1608a08395fc4955e1274278fc
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2022-07-04 16:18:20 +0800

    Update Sed.sh

--- Bash/Sed.sh
+++ Bash/Sed.sh
@@ -1,1 +1,2 @@
 sed 's/\<the\>/this/1' filename.txt # зөвхөн the гэдэг 3 үсэгтэй үг таарвал this болгон replace хийж байна 1 буюу эхний case-нд
+sed 's/\<thy\>/your/gi' filename.txt # <g> бүх таарах thy, <i> нь бүх case insensitive нөхцөлд replace хийнэ. 
