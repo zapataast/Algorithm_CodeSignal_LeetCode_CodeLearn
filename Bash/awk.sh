fe3297c6c66e0b565e1c51a4e1fafb105d6027e8
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2022-07-20 15:32:34 +0800

    Update awk.sh

--- Bash/awk.sh
+++ Bash/awk.sh
@@ -1,2 +1,3 @@
 awk '$1 >200' employee.txt # эхний баганын id 200 гаас дээш байвал ялгаж харуулна. 
-awk '$4 ~/Technology/' employee.txt # 4 дэхь багананд Technology нэртэй ажилчид байвал ялгаж харуулна. 
+awk '$4 ~/Technology/' employee.txt # 4 дэхь багананд Technology нэртэй ажилчид байвал ялгаж харуулна.
+awk '/Manager/ { print }' employee.txt # гэвэл Manager гэсэн үг орсон мөрнүүдийг ялгаж xаруулна. 
