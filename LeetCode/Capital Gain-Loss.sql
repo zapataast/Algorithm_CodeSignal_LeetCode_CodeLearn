0ed358067f48f3a8e9784787f6ec53b0aa83b0ff
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2023-03-27 16:42:27 +0800

    1393. Capital Gain/Loss

---
+++ LeetCode/Capital Gain-Loss.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+SELECT stock_name, 
+    SUM(CASE WHEN operation = 'Sell' THEN price 
+    WHEN operation = 'Buy' THEN -price
+    END) as capital_gain_loss 
+FROM Stocks
+Group by stock_name
