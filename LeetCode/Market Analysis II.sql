935c4701a31eaa3fc3a64f09a36e509459dfd1e7
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2023-03-27 16:22:35 +0800

    9. Market Analysis II

---
+++ LeetCode/Market Analysis II.sql
@@ -0,0 +1,7 @@
+SELECT u1.user_id as buyer_id, u1.join_date ,ISNULL(uu.orders_in_2019,0) as orders_in_2019
+FROM Users as u1 left join (SELECT buyer_id,count(*) as orders_in_2019
+FROM Orders
+where year(order_date) = 2019 
+GROUP BY buyer_id) as uu on u1.user_id = uu.buyer_id
+
+
